c74e54537b4fda5ab479f1a072bbad6922898b06
kjstyle <kjstyle79@naver.com>
2024-09-27 16:33:44 +0900

    테스트가 돌때마다 테스트 이미지들이 누적해서 쌓여서 청소하는 코드 추가

--- src/test/java/kjstyle/accommodation/domain/UploadServiceTest.java
+++ src/test/java/kjstyle/accommodation/domain/UploadServiceTest.java
@@ -5,19 +5,20 @@ import kjstyle.accommodation.domain.repository.ImageRepository;
 import kjstyle.accommodation.domain.repository.common.BaseTest;
 import kjstyle.accommodation.domain.repository.entities.ImageEntity;
 import kjstyle.accommodation.domain.service.UploadService;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-
 
 import java.io.IOException;
-
-import static org.mockito.ArgumentMatchers.any;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class UploadServiceTest extends BaseTest {
@@ -28,6 +29,8 @@ class UploadServiceTest extends BaseTest {
     @MockBean
     private ImageRepository imageRepository;
 
+    private static final String TEMP_PATH = "tmpfile/accommodation/MAIN";
+
     @Test
     void 이미지_업로드_테스트() throws IOException {
 
@@ -35,7 +38,7 @@ class UploadServiceTest extends BaseTest {
         ImageEntity mockImageEntity = ImageEntity.builder()
                 .id(123L)
                 .imageType(ImageType.MAIN)
-                .path("accommodation/main/12345.jpg")
+                .path(TEMP_PATH+"/12345.jpg")
                 .build();
 
         when(imageRepository.save(any(ImageEntity.class))).thenReturn(mockImageEntity);
@@ -44,9 +47,26 @@ class UploadServiceTest extends BaseTest {
 
         assertThat(savedImageEntity).isNotNull();
         assertThat(savedImageEntity.getImageType()).isEqualTo(ImageType.MAIN);
-        assertThat(savedImageEntity.getPath()).isEqualTo("accommodation/main/12345.jpg");
+        assertThat(savedImageEntity.getPath()).isEqualTo(TEMP_PATH + "/12345.jpg");
         assertThat(savedImageEntity.getId()).isEqualTo(123L);
 
         verify(imageRepository, times(1)).save(any(ImageEntity.class));
+    }
+
+
+    @AfterAll
+    static void cleanUpAll() throws Exception {
+        // 테스트로 저장했던 임시파일들 삭제 (안그럼 계속 누적될거라...)
+        Path tempDir = Paths.get(TEMP_PATH);
+        // 디렉토리가 존재하면 파일 삭제
+        if (Files.exists(tempDir)) {
+            Files.walk(tempDir)
+                    .map(Path::toFile)
+                    .forEach(file -> {
+                        if (!file.delete()) {
+                            System.err.println("Failed to delete file: " + file);
+                        }
+                    });
+        }
     }
 }
--- src/test/java/kjstyle/accommodation/web/ImageUploadRestControllerTest.java
+++ src/test/java/kjstyle/accommodation/web/ImageUploadRestControllerTest.java
@@ -1,17 +1,22 @@
 package kjstyle.accommodation.web;
 
 import kjstyle.accommodation.domain.repository.common.BaseMockMvcTest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ImageUploadRestControllerTest extends BaseMockMvcTest {
+
+    private static final String TEMP_PATH = "tmpfile/accommodation/MAIN";
 
     @Test
     void 이미지업로드_컨트롤러_테스트() throws Exception {
@@ -38,5 +43,21 @@ class ImageUploadRestControllerTest extends BaseMockMvcTest {
                                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 )
                 .andExpect(status().isBadRequest());
+    }
+
+    @AfterAll
+    static void cleanUpAll() throws Exception {
+        // 테스트로 저장했던 임시파일들 삭제 (안그럼 계속 누적될거라...)
+        Path tempDir = Paths.get(TEMP_PATH);
+        // 디렉토리가 존재하면 파일 삭제
+        if (Files.exists(tempDir)) {
+            Files.walk(tempDir)
+                    .map(Path::toFile)
+                    .forEach(file -> {
+                        if (!file.delete()) {
+                            System.err.println("Failed to delete file: " + file);
+                        }
+                    });
+        }
     }
 }
