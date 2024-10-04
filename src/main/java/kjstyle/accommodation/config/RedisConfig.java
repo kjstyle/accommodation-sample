c0aa9d7734a84c62035b9157d19d21dbba80cda5
kjstyle <kjstyle79@naver.com>
2024-10-04 18:37:11 +0900

    레디스 직렬화 관련 코드 원복 (이게 범인이 아니었음)

--- src/main/java/kjstyle/accommodation/config/RedisConfig.java
+++ src/main/java/kjstyle/accommodation/config/RedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching
 @Configuration
@@ -34,15 +34,8 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-
-        // jackson에서 is머시기 불린형 변수일 때 deserialize 때 문제가 생겨서..그냥 아래처럼 모두 string으로 처리하도록 변경
-        redisTemplate.setDefaultSerializer(RedisSerializer.string());
-        redisTemplate.setStringSerializer(RedisSerializer.string());
-        redisTemplate.setKeySerializer(RedisSerializer.string());
-        redisTemplate.setValueSerializer(RedisSerializer.string());
-        redisTemplate.setHashKeySerializer(RedisSerializer.string());
-        redisTemplate.setHashValueSerializer(RedisSerializer.string());
-
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
